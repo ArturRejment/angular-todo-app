57076df8b6c78e4424c5e882403a45342b656d29
Artur Rejment <arturrejment1@gmail.com>
2022-04-30 18:51:20 +0200

    feat: create Task interface

---
+++ angular-todo/src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+	{
+		id: 1,
+		text: 'Doctors appointment',
+		day: 'May 5th at 2:30pm',
+		reminder: true,
+	},
+	{
+		id: 2,
+		text: 'Test appointment',
+		day: 'Feb 5th at 5:00pm',
+		reminder: true,
+	},
+	{
+		id: 3,
+		text: 'Doctors hello',
+		day: 'May 5th a 1:30am',
+		reminder: false,
+	},
+]
